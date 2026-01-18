bdf9b3845cb24203042af42acf6561e4bae962b8
Vivek verma <133903913+vivekverma4669@users.noreply.github.com>
2026-01-19 00:52:12 +0530

    update icons

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -1,53 +1,201 @@
-import { useContext } from 'react';
-import './tech.css';
-import { AuthTheme } from '../AuthThemeProvider';
+import { useContext } from "react";
+import "./tech.css";
+import { AuthTheme } from "../AuthThemeProvider";
 const Tech = () => {
-  const{theme} = useContext(AuthTheme);
-    return (
-      <div style={{backgroundColor : theme=='day'? null : 'rgb(16, 22,34)'  ,marginTop: "-50px"}}>
-     <h1 className='techS' id='techS'  style={{textAlign:"center",  color: theme=='day'? "rgb(0, 85, 169)" : '#32de84' ,  textShadow :"2px 2px 2px black", fontSize:"50px", }}><ul>Tech Stack</ul></h1>
-  
+  const { theme } = useContext(AuthTheme);
+  return (
+    <div
+      style={{
+        backgroundColor: theme == "day" ? null : "rgb(16, 22,34)",
+        marginTop: "-50px",
+      }}
+    >
+      <h1
+        className="techS"
+        id="techS"
+        style={{
+          textAlign: "center",
+          color: theme == "day" ? "rgb(0, 85, 169)" : "#32de84",
+          textShadow: "2px 2px 2px black",
+          fontSize: "50px",
+        }}
+      >
+        <ul>Tech Stack</ul>
+      </h1>
 
-       <fieldset style={{backgroundColor : theme=='day'? null : 'rgb(16, 22,34)'}}>
-       <legend> <h2 style={{textAlign:"center",  color: theme=='day'? "rgb(0, 85, 169)": 'rgb(42, 205, 216)',  textShadow :"2px 2px 2px black" ,fontSize:"50px" ,backgroundColor : theme=='day'? null : 'rgb(16, 22,34)'  }} className='stack'>Languages & Skills  </h2></legend>
-        <div className="container2" style={{ backgroundColor : theme=='day'? null : 'rgb(16, 22,34)'}}>
-        <div className='card'><img src="https://icon-library.com/images/html5-icon/html5-icon-13.jpg"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>HTML</h3></div>
-        <div className='card'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/768px-CSS3_logo.svg.png?20210705212817p"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>CSS</h3></div>
-        <div className='card'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/768px-Unofficial_JavaScript_logo_2.svg.png?20141107110902"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Java Script</h3></div>
-        <div className='card'><img src="https://cdn.worldvectorlogo.com/logos/react-1.svg"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>React</h3></div>
-        <div className='card'><img src="https://w7.pngwing.com/pngs/915/519/png-transparent-typescript-hd-logo-thumbnail.png"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Type Script</h3></div>
-        <div className='card'><img src="https://cdn.worldvectorlogo.com/logos/next-js.svg" style={{filter : theme=='day'? 'inherit' : "invert()" }}/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Next js</h3></div>
-        <div className='card'><img src="https://seeklogo.com/images/J/java-logo-7F8B35BAB3-seeklogo.com.png"/> <h3 style={{color : theme=='day'? 'black' : 'white' , }}>Java</h3></div>
-        <div className='card'><img src="https://www.svgrepo.com/show/439231/mongodb.svg"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Mongol DB</h3></div>
-        <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/919/919825.png"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Node js</h3></div>
-        <div className='card'><img src="https://youteam.io/blog/wp-content/uploads/2022/04/expressjs_logo.png"/> <h3  style={{color : theme=='day'? 'black' : 'white'  }}>Express js</h3></div>
-        <div className='card'><img src="https://cdn.iconscout.com/icon/free/png-256/free-redux-3629610-3032308.png"  style={{filter : theme=='day'? 'inherit' : "invert()"}}/> <h3 style={{color : theme=='day'? 'black' : 'white'  }}>Redux</h3></div>
-        <div className='card'><img src="https://brandlogos.net/wp-content/uploads/2021/09/bootstrap-logo.png"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Boot strap</h3></div>
-        <div className='card'><img src="https://www.svgrepo.com/show/354262/react-router.svg"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Rauter</h3></div>
-        <div className='card'><img src="https://seeklogo.com/images/P/passport-logo-16D89B2F37-seeklogo.com.png"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Passport</h3></div>
+      <fieldset
+        style={{ backgroundColor: theme == "day" ? null : "rgb(16, 22,34)" }}
+      >
+        <legend>
+          {" "}
+          <h2
+            style={{
+              textAlign: "center",
+              color: theme == "day" ? "rgb(0, 85, 169)" : "rgb(42, 205, 216)",
+              textShadow: "2px 2px 2px black",
+              fontSize: "50px",
+              backgroundColor: theme == "day" ? null : "rgb(16, 22,34)",
+            }}
+            className="stack"
+          >
+            Languages & Skills{" "}
+          </h2>
+        </legend>
+        <div
+          className="container2"
+          style={{ backgroundColor: theme == "day" ? null : "rgb(16, 22,34)" }}
+        >
+          <div className="card">
+            <img src="https://icon-library.com/images/html5-icon/html5-icon-13.jpg" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>HTML</h3>
+          </div>
+          <div className="card">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/768px-CSS3_logo.svg.png?20210705212817p" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>CSS</h3>
+          </div>
+          <div className="card">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/768px-Unofficial_JavaScript_logo_2.svg.png?20141107110902" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Java Script
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://cdn.worldvectorlogo.com/logos/react-1.svg" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>React</h3>
+          </div>
+          <div className="card">
+            <img src="https://w7.pngwing.com/pngs/915/519/png-transparent-typescript-hd-logo-thumbnail.png" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Type Script
+            </h3>
+          </div>
+          <div className="card">
+            <img
+              src="https://cdn.worldvectorlogo.com/logos/next-js.svg"
+              style={{ filter: theme == "day" ? "inherit" : "invert()" }}
+            />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Next js
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/1200/external-java-is-a-general-purpose-programming-language-that-is-class-based-logo-shadow-tal-revivo.jpg" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>Java</h3>
+          </div>
+          <div className="card">
+            <img src="https://www.svgrepo.com/show/439231/mongodb.svg" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Mongol DB
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://cdn-icons-png.flaticon.com/512/919/919825.png" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Node js
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://img.icons8.com/ios7/1200/express-js.jpg" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Express js
+            </h3>
+          </div>
+          <div className="card">
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-256/free-redux-3629610-3032308.png"
+              style={{ filter: theme == "day" ? "inherit" : "invert()" }}
+            />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>Redux</h3>
+          </div>
+          <div className="card">
+            <img src="https://brandlogos.net/wp-content/uploads/2021/09/bootstrap-logo.png" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Boot strap
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://www.svgrepo.com/show/354262/react-router.svg" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Rauter
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://avatars.githubusercontent.com/u/1160530?s=280&v=4" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Passport
+            </h3>
+          </div>
         </div>
-        </fieldset>
- 
+      </fieldset>
 
-        <fieldset style={{ backgroundColor : theme=='day'? null : 'rgb(16, 22,34)' }}>
-       <legend> <h2 style={{textAlign:"center",  color: theme=='day'? "rgb(0, 85, 169)": 'rgb(42, 205, 216)',  textShadow :"2px 2px 2px black" ,fontSize:"50px"  }} className='stack'>Tools </h2></legend>
-        <div className="container2" style={{backgroundColor : theme=='day'? null : 'rgb(16, 22,34)'}} >
-        <div className='card'><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" style={{filter : theme=='day'? 'inherit' : "invert()"}}/> <h3 style={{color : theme=='day'? 'black' : 'white'  }}>Git hub</h3></div>
-        <div className='card'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png"/> <h3 style={{color : theme=='day'? 'black' : 'white'}} >Git</h3></div>
-        <div className='card'><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1024px-ChatGPT_logo.svg.png"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Chat GPT</h3></div>
-        <div className='card'><img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/netlify_logo_icon_169923.png"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Netify</h3></div>
-        <div className='card'><img src="https://www.svgrepo.com/show/354202/postman-icon.svg"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>postman </h3></div>
-        <div className='card'><img src="https://d6xcmfyh68wv8.cloudfront.net/assets/razorpay-glyph.svg"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>razorpay</h3></div>
-        <div className='card'><img src="https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png"/> <h3 style={{color : theme=='day'? 'black' : 'white'}}>Firebase</h3></div>
-        
+      <fieldset
+        style={{ backgroundColor: theme == "day" ? null : "rgb(16, 22,34)" }}
+      >
+        <legend>
+          {" "}
+          <h2
+            style={{
+              textAlign: "center",
+              color: theme == "day" ? "rgb(0, 85, 169)" : "rgb(42, 205, 216)",
+              textShadow: "2px 2px 2px black",
+              fontSize: "50px",
+            }}
+            className="stack"
+          >
+            Tools{" "}
+          </h2>
+        </legend>
+        <div
+          className="container2"
+          style={{ backgroundColor: theme == "day" ? null : "rgb(16, 22,34)" }}
+        >
+          <div className="card">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+              style={{ filter: theme == "day" ? "inherit" : "invert()" }}
+            />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Git hub
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/1200px-Git_icon.svg.png" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>Git</h3>
+          </div>
+          <div className="card">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/ChatGPT_logo.svg/1024px-ChatGPT_logo.svg.png" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Chat GPT
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://cdn.icon-icons.com/icons2/2699/PNG/512/netlify_logo_icon_169923.png" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Netify
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://www.svgrepo.com/show/354202/postman-icon.svg" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              postman{" "}
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://d6xcmfyh68wv8.cloudfront.net/assets/razorpay-glyph.svg" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              razorpay
+            </h3>
+          </div>
+          <div className="card">
+            <img src="https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png" />{" "}
+            <h3 style={{ color: theme == "day" ? "black" : "white" }}>
+              Firebase
+            </h3>
+          </div>
         </div>
-        </fieldset>
+      </fieldset>
+    </div>
+  );
+};
 
-       
-       
-      </div>
-    );
-  };
-  
-  export default Tech;
-  
+export default Tech;
--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,88 +1,188 @@
-import React, { useContext } from 'react';
-import './contact.css';
-import { AuthTheme } from '../AuthThemeProvider';
-import Resume from './Videos/Vivek-Verma-Resume.pdf';
+import React, { useContext } from "react";
+import "./contact.css";
+import { AuthTheme } from "../AuthThemeProvider";
+import Resume from "./Videos/Vivek-Verma-Resume.pdf";
 
+const Contact = () => {
+  const { theme } = useContext(AuthTheme);
+  return (
+    <>
+      <div
+        className="box5 reveal"
+        id="box5"
+        style={{ backgroundColor: theme == "day" ? "#aaaeb0" : "#051c26" }}
+      >
+        <div className="box51" id="contact">
+          <h1
+            style={{
+              textAlign: "center",
+              color: theme == "day" ? "rgb(0, 85, 169)" : "#32de84",
+              textShadow: "2px 2px 2px black",
+              fontSize: "50px",
+            }}
+          >
+            CONTACT
+          </h1>
+        </div>
 
-   const Contact = () => {
-    const{theme} = useContext(AuthTheme);
-     return (
-       <>
-         <div className="box5 reveal" id="box5"  style={{backgroundColor : theme=='day'?  '#aaaeb0' : '#051c26' }}>  
+        <div className="box52">
+          <div className="box511">
+            <h1
+              className="box5h1"
+              style={{ color: theme == "day" ? null : "white" }}
+            >
+              Reach Out to me!
+            </h1>
 
-         <div className="box51" id='contact'>
-          
-           <h1 style={{textAlign:"center",  color: theme=='day'? "rgb(0, 85, 169)" : '#32de84' ,  textShadow :"2px 2px 2px black", fontSize:"50px", }}>CONTACT</h1>
+            <div className="box5111">
+              <p style={{ color: theme == "day" ? null : "white" }}>
+                <i className="fa-solid fa-location-dot"></i> Prayagraj, Uttar
+                Pradesh
+              </p>
+            </div>
+
+            <div className="box5111">
+              <p
+                id="contact-email"
+                style={{ color: theme == "day" ? null : "white" }}
+              >
+                <i className="fa-solid fa-envelope"></i>vivekverma4679@gmail.com
+              </p>
+            </div>
+
+            <div className="box5111">
+              <p style={{ color: theme == "day" ? null : "white" }}>
+                <i className="fa-solid fa-phone"></i> +91-9369546165
+              </p>
+            </div>
+
+            <div className="box5112">
+              <div className="ico">
+                <a href="https://github.com/vivekverma4669" id="contact-github">
+                  {" "}
+                  <img
+                    src="https://www.svgrepo.com/show/361181/github.svg"
+                    style={{
+                      width: "35px",
+                      filter: theme == "day" ? null : "invert(100%)",
+                    }}
+                  ></img>{" "}
+                </a>
+              </div>
+              <div className="ico">
+                <a href="https://wa.me/919369546165" id="contact-phone">
+                  {" "}
+                  <img
+                    src="https://assets.streamlinehq.com/image/private/w_300,h_300,ar_1/f_auto/v1/icons/logo/logo-whatsapp-esz7748uz5sohs2ktm4o.png/logo-whatsapp-6mihjwai8cvcdrwckxq459.png?_a=DATAiZAAZAA0"
+                    style={{
+                      width: "35px",
+                      filter: theme == "day" ? null : "invert(100%)",
+                    }}
+                  ></img>
+                </a>
+              </div>
+              <div className="ico">
+                <a
+                  href="https://www.linkedin.com/in/vivek-verma-594700228/"
+                  id="contact-linkedin"
+                >
+                  <img
+                    src="https://www.svgrepo.com/show/108614/linkedin.svg"
+                    style={{
+                      width: "35px",
+                      filter: theme == "day" ? null : "invert(100%)",
+                    }}
+                  ></img>
+                </a>
+              </div>
+              {/* <div className="ico"><a href="https://www.linkedin.com/in/vivek-verma-594700228/" id=''><img src='https://www.svgrepo.com/show/108614/linkedin.svg'  style={{width:"35px"}}></img></a></div> */}
+              <div className="ico">
+                <a href="mailto:vivekverma4679@gmail.com" id="contact-email">
+                  <img
+                    src="https://static.vecteezy.com/system/resources/thumbnails/018/931/264/small/black-mail-icon-png.png"
+                    style={{
+                      width: "40px",
+                      filter: theme == "day" ? null : "invert(100%)",
+                    }}
+                  ></img>
+                </a>
+              </div>
+              <div className="ico">
+                <a href="https://www.instagram.com/vivek__.soni/">
+                  {" "}
+                  <img
+                    src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/black-instagram-icon.png"
+                    style={{
+                      width: "35px",
+                      filter: theme == "day" ? null : "invert(100%)",
+                    }}
+                  ></img>
+                </a>
+              </div>
+              <div className="ico">
+                <a href="tel:+919369546165" id="contact-phone">
+                  <img
+                    src="https://www.iconpacks.net/icons/1/free-phone-icon-504-thumb.png"
+                    style={{
+                      width: "35px",
+                      filter: theme == "day" ? null : "invert(100%)",
+                    }}
+                  ></img>
+                </a>
+              </div>
+            </div>
           </div>
 
-   
-           <div className="box52">
+          <div className="box512">
+            <img
+              className="img51"
+              src="https://i.ibb.co/1r9Hhxs/Whats-App-Image-2024-01-17-at-20-04-51-344e3480.jpg"
+              alt=""
+            />
+            <div className="btn_div">
+              <button
+                className="rbtn"
+                style={{
+                  marginLeft: "-50px",
+                  margin: "10px",
+                  backgroundColor: theme == "day" ? null : "green",
+                }}
+              >
+                <a download="Vivek-Verma-Resume" href={Resume}>
+                  {" "}
+                  Resume{" "}
+                  <img
+                    src="https://icon-library.com/images/download-icon-white-png/download-icon-white-png-22.jpg"
+                    style={{ width: "30px" }}
+                    id="resume-button-1"
+                  />
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
 
-               <div className="box511">
-                 <h1 className="box5h1"  style={{color : theme=='day'? null: 'white'}}>Reach Out to me!</h1>
-               
-   
-               <div className="box5111">
-                 <p style={{color : theme=='day'? null: 'white'}}><i className="fa-solid fa-location-dot"></i> Prayagraj, Uttar Pradesh</p>
-               </div>
+      <h3
+        style={{
+          backgroundColor: "rgb(0, 85, 169",
+          margin: "0px",
+          textAlign: " center",
+          color: "white",
+        }}
+      >
+        Built From Scrach by Vivek 😎
+      </h3>
+    </>
+  );
+};
 
-               <div className="box5111">
-                 <p id="contact-email" style={{color : theme=='day'? null: 'white'}} ><i className="fa-solid fa-envelope"></i>vivekverma4679@gmail.com</p>
-               </div>
-
-               <div className="box5111">
-                 <p style={{color : theme=='day'? null: 'white'}}><i className="fa-solid fa-phone"></i> +91-9369546165</p>
-               </div>
-
-               
-   
-               <div className="box5112">
-                 <div className="ico" ><a href="https://github.com/vivekverma4669" id='contact-github'> <img src='https://www.svgrepo.com/show/361181/github.svg'  style={{width:"35px"  , filter: theme=='day'? null : "invert(100%)"}}></img>  </a></div>
-                 <div className="ico"><a href="https://wa.me/919369546165" id="contact-phone"> <img src='https://static-00.iconduck.com/assets.00/whatsapp-icon-2038x2048-5zk5gj1j.png'  style={{width:"35px" , filter: theme=='day'? null : "invert(100%)"}}></img></a></div>
-                 <div className="ico"><a href="https://www.linkedin.com/in/vivek-verma-594700228/" id='contact-linkedin'><img src='https://www.svgrepo.com/show/108614/linkedin.svg'  style={{width:"35px" ,filter: theme=='day'? null : "invert(100%)"}}></img></a></div>
-                 {/* <div className="ico"><a href="https://www.linkedin.com/in/vivek-verma-594700228/" id=''><img src='https://www.svgrepo.com/show/108614/linkedin.svg'  style={{width:"35px"}}></img></a></div> */}
-                 <div className="ico"><a href="mailto:vivekverma4679@gmail.com" id='contact-email'><img src='https://www.svgrepo.com/show/1284/message.svg'  style={{width:"35px",filter: theme=='day'? null : "invert(100%)"}}></img></a></div>
-                 <div className="ico"><a href="https://www.instagram.com/vivek__.soni/"> <img src='https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/black-instagram-icon.png'  style={{width:"35px" , filter: theme=='day'? null : "invert(100%)"}} ></img></a></div>
-                 <div className="ico"><a href="tel:+919369546165" id='contact-phone'><img src='https://www.iconpacks.net/icons/1/free-phone-icon-504-thumb.png'  style={{width:"35px" , filter: theme=='day'? null : "invert(100%)"}}></img></a></div>
-               </div>
-              
-             
-               
-               
-               </div>
-       
-   
-         <div className="box512">
-         <img className="img51" src="https://i.ibb.co/1r9Hhxs/Whats-App-Image-2024-01-17-at-20-04-51-344e3480.jpg" alt="" />
-         <div className="btn_div">
-
-         <button className="rbtn"  style={{ marginLeft :"-50px", margin: "10px" ,backgroundColor :theme=='day'? null: "green"}} >
-        <a download="Vivek-Verma-Resume" href={Resume}> Resume <img src='https://icon-library.com/images/download-icon-white-png/download-icon-white-png-22.jpg' style={{width :"30px"}}   id="resume-button-1"/></a>
-        </button>
-
-         </div>
-         </div>
-
-</div>
-
- 
-         </div>  
-        
-         <h3 style={{backgroundColor :'rgb(0, 85, 169' , margin :'0px' , textAlign:' center' , color: 'white' }}>Built From Scrach by  Vivek 😎</h3>
-            
-       </>
-     );
-   };
-   
-   export default Contact;
-   
-
+export default Contact;
 
 //    <div class="box5 reveal" id="box5">
 //    <div class="box51">
 //        <h1>CONTACT</h1>
 //    </div>
-
 
 //    <div class="box52">
 
@@ -91,8 +191,6 @@ import Resume from './Videos/Vivek-Verma-Resume.pdf';
 //            <h1 class="box5h1">Reach Out to me!</h1>
 
 //            <h3 class="box5h3">DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.</h3>
-
-           
 
 //            <div class="box5111">
 //                <p><i class="fa-solid fa-location-dot"></i> Barmer, Rajasthan</p>
@@ -113,11 +211,8 @@ import Resume from './Videos/Vivek-Verma-Resume.pdf';
 //                <div class="ico"><a href="https://wa.me/918696700323"><i class="fa-brands fa-whatsapp"></i></a></div>
 //            </div>
 
-           
-
 //        </div>
 
-                   
 //        <div class="box512">
 //            <img class="img51" src="image/img2.png" alt="">
 //            <div class="btn_div">
@@ -126,8 +221,5 @@ import Resume from './Videos/Vivek-Verma-Resume.pdf';
 //        </div>
 
 //    </div>
-   
-
 
 // </div>
-
